815a04958b5b0d22bc41d4a6cd33b270906f4864
Fellipe Caetano <fellipe.caetano4@gmail.com>
2020-08-10 14:54:07 -0300

    Re-create project inside workspace

---
+++ Project/Application/Application/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Application
+//
+//  Created by fellipe caetano on 10/08/20.
+//  Copyright © 2020 Fellipe Caetano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
