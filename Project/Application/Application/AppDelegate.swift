9b293cd0764dd100f2248f3d0b2d10596d5de212
Fellipe Caetano <fellipe.caetano4@gmail.com>
2020-08-10 16:24:36 -0300

    Create SignInViewController

--- Project/Application/Application/AppDelegate.swift
+++ Project/Application/Application/AppDelegate.swift
@@ -2,12 +2,13 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = SignInViewController()
+        window?.makeKeyAndVisible()
         return true
-    }
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 }
 
---
+++ Project/Application/Application/SignInViewController.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+class SignInViewController: UIViewController {
+    override func loadView() {
+        view = SignInView()
+    }
+}
