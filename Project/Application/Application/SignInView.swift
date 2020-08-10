fc88d0593bc902f44372c99e70ff9a5d411c7534
Fellipe Caetano <fellipe.caetano4@gmail.com>
2020-08-10 16:34:58 -0300

    Update SignInView.swift
    Add text field for the email address

--- Project/Application/Application/SignInView.swift
+++ Project/Application/Application/SignInView.swift
@@ -1,6 +1,19 @@
 import UIKit
 
 final class SignInView: UIView {
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "E-mail"
+        textField.keyboardType = .emailAddress
+        textField.autocapitalizationType = .none
+        
+        if #available(iOS 10.0, *) {
+            textField.textContentType = .emailAddress
+        }
+        
+        return textField
+    }()
+    
     init() {
         super.init(frame: UIScreen.main.bounds)
         setUp()
@@ -11,6 +24,27 @@ final class SignInView: UIView {
     }
     
     private func setUp() {
-        backgroundColor = .gray
+        configure()
+        createHierarchy()
+        createConstraints()
+    }
+    
+    private func configure() {
+        backgroundColor = .white
+    }
+    
+    private func createHierarchy() {
+        addSubview(emailTextField)
+    }
+    
+    private func createConstraints() {
+        emailTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            emailTextField.centerYAnchor.constraint(equalTo: centerYAnchor),
+            emailTextField.centerYAnchor.constraint(equalTo: centerYAnchor),
+            emailTextField.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor),
+            emailTextField.leadingAnchor.constraint(equalTo: layoutMarginsGuide.leadingAnchor)
+        ])
     }
 }
