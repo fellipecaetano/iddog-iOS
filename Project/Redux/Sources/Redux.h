1f3c86486983d53549071dad51c6e6d211993f6f
Fellipe Caetano <fellipe.caetano4@gmail.com>
2020-08-10 23:59:16 -0300

    Create Redux.framework

---
+++ Project/Redux/Sources/Redux.h
@@ -0,0 +1,19 @@
+//
+//  Redux.h
+//  Redux
+//
+//  Created by fellipe caetano on 10/08/20.
+//  Copyright © 2020 Fellipe Caetano. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Redux.
+FOUNDATION_EXPORT double ReduxVersionNumber;
+
+//! Project version string for Redux.
+FOUNDATION_EXPORT const unsigned char ReduxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Redux/PublicHeader.h>
+
+
