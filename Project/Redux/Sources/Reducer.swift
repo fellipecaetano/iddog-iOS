24a52d8ce5db1deffb9ef0f88487dfa816e28b2a
Fellipe Caetano <fellipe.caetano4@gmail.com>
2020-08-13 13:10:48 -0300

    Update Reducer.swift
    Add "combine" and "pullback" functions

--- Project/Redux/Sources/Reducer.swift
+++ Project/Redux/Sources/Reducer.swift
@@ -6,4 +6,44 @@ public struct Reducer<State, Action, Environment> {
     public init(reduce: @escaping (inout State, Action, Environment) -> Effect<Action>) {
         self.reduce = reduce
     }
+
+    func pullback<GlobalState, GlobalAction, GlobalEnvironment>(
+        state toLocalState: WritableKeyPath<GlobalState, State>,
+        toLocalAction: @escaping (GlobalAction) -> Action?,
+        fromLocalAction: @escaping (Action) -> GlobalAction,
+        environment toLocalEnvironment: @escaping (GlobalEnvironment) -> Environment
+    ) -> Reducer<GlobalState, GlobalAction, GlobalEnvironment> {
+        return Reducer<GlobalState, GlobalAction, GlobalEnvironment> { globalState, globalAction, globalEnvironment in
+            guard let localAction = toLocalAction(globalAction) else {
+                return Effect.empty
+            }
+
+            return self.reduce(
+                &globalState[keyPath: toLocalState],
+                localAction,
+                toLocalEnvironment(globalEnvironment)
+            )
+            .map(fromLocalAction)
+            .eraseToEffect()
+        }
+    }
+
+    static func combine(_ reducers: Reducer...) -> Reducer {
+        return Self { state, action, environment in
+            let effects: [Effect<Action>] = reducers.map { $0.reduce(&state, action, environment) }
+
+            return AnyObservable<Action> { onComplete in
+                let disposables = effects.map { effect in
+                    effect.subscribe(onComplete)
+                }
+
+                return Disposable {
+                    for disposable in disposables {
+                        disposable.dispose()
+                    }
+                }
+            }
+            .eraseToEffect()
+        }
+    }
 }
