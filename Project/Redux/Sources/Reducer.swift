b5de9355257a145b5503fdfb163493b48d6ddc70
Fellipe Caetano <fellipe.caetano4@gmail.com>
2020-08-13 12:22:10 -0300

    Add environment to reducer

--- Project/Redux/Sources/Reducer.swift
+++ Project/Redux/Sources/Reducer.swift
@@ -1,5 +1,9 @@
 import Streams
 
-public struct Reducer<State, Action> {
-    let reduce: (inout State, Action) -> Effect<Action>
+public struct Reducer<State, Action, Environment> {
+    public let reduce: (inout State, Action, Environment) -> Effect<Action>
+
+    public init(reduce: @escaping (inout State, Action, Environment) -> Effect<Action>) {
+        self.reduce = reduce
+    }
 }
--- Project/Redux/Sources/Store.swift
+++ Project/Redux/Sources/Store.swift
@@ -1,6 +1,6 @@
 import Streams
 
-public final class Store<State: Equatable, Action>: Observable, Observer {
+public final class Store<State: Equatable, Action, Environment>: Observable, Observer {
     public private(set) var state: State {
         didSet {
             if oldValue != state {
@@ -9,13 +9,15 @@ public final class Store<State: Equatable, Action>: Observable, Observer {
         }
     }
 
-    private let reducer: Reducer<State, Action>
+    private let reducer: Reducer<State, Action, Environment>
+    private let environment: Environment
     private let subject = Subject<State>()
     private var effectDisposables: [UUID: Disposable] = [:]
 
-    init(initialState: State, reducer: Reducer<State, Action>) {
+    init(initialState: State, reducer: Reducer<State, Action, Environment>, environment: Environment) {
         state = initialState
         self.reducer = reducer
+        self.environment = environment
     }
 
     public func subscribe(_ handler: @escaping (State) -> Void) -> Disposable {
@@ -24,7 +26,7 @@ public final class Store<State: Equatable, Action>: Observable, Observer {
     }
 
     public func emit(_ action: Action) {
-        let effect = reducer.reduce(&state, action)
+        let effect = reducer.reduce(&state, action, environment)
         let uuid = UUID()
         let disposable = effect.subscribe { action in
             self.emit(action)
