dac5660ffc9506f3598055cb116f45cd635dcc96
Fellipe Caetano <fellipe.caetano4@gmail.com>
2020-08-14 15:09:28 -0300

    Add collection view to FeedView

---
+++ Project/Application/Sources/Core/UIView+SafeArea.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+extension UIView {
+    var safeTopAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.topAnchor
+        } else {
+            return topAnchor
+        }
+    }
+    
+    var safeBottomAnchor: NSLayoutYAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.bottomAnchor
+        } else {
+            return bottomAnchor
+        }
+    }
+    
+    var safeLeadingAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.leadingAnchor
+        } else {
+            return leadingAnchor
+        }
+    }
+
+    var safeTrailingAnchor: NSLayoutXAxisAnchor {
+        if #available(iOS 11.0, *) {
+            return safeAreaLayoutGuide.trailingAnchor
+        } else {
+            return trailingAnchor
+        }
+    }
+}
--- Project/Application/Sources/Feed/FeedView.swift
+++ Project/Application/Sources/Feed/FeedView.swift
@@ -1,6 +1,21 @@
 import UIKit
 
 final class FeedView: UIView {
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumLineSpacing = 16
+        layout.minimumInteritemSpacing = 16
+        layout.itemSize = CGSize(width: 500, height: 400)
+
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .white
+        collectionView.alwaysBounceVertical = false
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.showsVerticalScrollIndicator = false
+        return collectionView
+    }()
+
     init() {
         super.init(frame: UIScreen.main.bounds)
         setUp()
@@ -11,6 +26,22 @@ final class FeedView: UIView {
     }
 
     private func setUp() {
-        backgroundColor = .cyan
+        createHierarchy()
+        createConstraints()
+    }
+    
+    private func createHierarchy() {
+        addSubview(collectionView)
+    }
+
+    private func createConstraints() {
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: safeTopAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: safeBottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: safeLeadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: safeTrailingAnchor)
+        ])
     }
 }
